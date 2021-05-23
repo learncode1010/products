1303da22cdcc2299f00449fea8b16480728a594f
Brian Su <ccsu1010@gmail.com>
2021-05-23 17:00:37 +0800

    version 2.0

--- products.py
+++ products.py
@@ -1,4 +1,19 @@
+import os
+
 products = []
+if os.path.isfile('products.txt'):
+    print('讀入資料.....')
+    with open('products.txt', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip('\n').split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('建立新資料.....')
+
+
 while True:
 	name = input('請輸入消費商品 (離開輸入 q): ')
 	if name == 'q':
@@ -8,8 +23,9 @@ while True:
 	products.append(p)
 print(products)
 
+
 with open('products.txt', 'w', encoding = 'utf-8') as f:
-	f.write('商品, 價格\n')
+	f.write('商品,價格\n')
 	for product in products:
 		f.write(product[0] + ',' + product[1] + '\n')
 
